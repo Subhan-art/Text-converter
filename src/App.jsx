a1c2ac77194fdacbb1ebc0b50a9eff6b535d9401
Subhan-art <subhanaqeel305@gmail.com>
2024-11-09 07:49:47 -0800

    edit lines

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,7 @@ import Navbar from './component/Navbar'
 import TextForm from './component/Textform'
 
 function App() {
-  
   return (
-
     <>
       <Navbar title="textUtils" />
       <div className='container'>
